9e18f7433db147239eece230cac9290f67c62493
Iris Bombeke <iris.bombeke@hotmail.com>
2026-08-27 12:50:27 +0200

    Adapt data.py and train.pyto include the latest tuned RF model for deployment

--- src/train.py
+++ src/train.py
@@ -3,62 +3,74 @@ import joblib
 import mlflow
 import mlflow.sklearn
 import pandas as pd
+import numpy as np
+import pathlib
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error, r2_score
+from sklearn.model_selection import KFold, cross_val_score
+from mlflow.models import infer_signature
 
 # Import the data loader from our new module
-from data import load_and_split_data
+from data import load_data
+
+# Point to shared MLflow database in project folder root
+project_root = pathlib.Path(__file__).resolve().parent.parent
+db_path = project_root / "mlflow.db"
+mlflow.set_tracking_uri(f"sqlite:///{db_path}")
+mlflow.set_experiment("LNP_Encapsulation_Prediction")
 
 def train_model():
-    # 1. Set the MLflow Experiment
-    mlflow.set_experiment("LNP_Encapsulation_Prediction")
-    mlflow.set_tracking_uri("sqlite:///mlflow.db")
 
-    # 2. Start the tracking context
-    with mlflow.start_run(run_name="Baseline_RandomForest_Model2.2"):
-        
-        # Load the actual processed dataset
-        print("Loading data...")
-        X_train, X_test, y_train, y_test = load_and_split_data(filepath="../data/processed/df_final_num_cat.csv")
-        
-        # Define and log parameters
-        random_state = 126
-        mlflow.log_param("model_type", "RandomForestRegressor")
-        mlflow.log_param("random_state", random_state)
-        mlflow.log_param("features", "Numerical + Ionizable Lipid")
-        
-        # Train model
+    #Load full dataset (no splitting)
+    X, y = load_data()
+    print("Loading Data...")
+
+    # Define Model and parameters
+    rf_model = RandomForestRegressor(
+        n_estimators=100, 
+        max_depth=None, 
+        min_samples_split=10, 
+        random_state=126
+    )
+
+    # Start the tracking context
+    with mlflow.start_run(run_name="RandomForest_Tuned"):
+
+        # --- STEP 1: EVALUATION (For MLFlow Tracking) ---
+        kf = KFold(n_splits=5, shuffle=True, random_state=126)
+        mae_scores = -cross_val_score(rf_model, X, y, cv=kf, scoring='neg_mean_absolute_error')
+        r2_scores = cross_val_score(rf_model, X, y, cv=kf, scoring='r2')
+
+        # Log metrics to MLFlow
+        mlflow.log_metric("cv_mean_mae", np.mean(mae_scores))
+        mlflow.log_metric("cv_mean_r2", np.mean(r2_scores))
+
+        # Log parameters to MLFlow
+        mlflow.log_param("n_features", X.shape[1])
+        mlflow.log_params(rf_model.get_params())
+        mlflow.log_param("cv_folds", 5)
+
+        #Log dataset attribute           
+        dataset_df = X.copy()
+        dataset_df["encapsulation_efficiency"] = y
+        dataset = mlflow.data.from_pandas(dataset_df, targets="encapsulation_efficiency", name="advanced_all_features")
+        mlflow.log_input(dataset, context="training")
+    
+        # --- STEP 2: DEPLOYMENT (For FastAPI) --- 
+        # Train model on full dataset
+        rf_model.fit(X, y)
         print("Training Random Forest model...")
-        rf_model = RandomForestRegressor(random_state=random_state)
-        rf_model.fit(X_train, y_train)
-        
-        # Evaluate model
-        print("Evaluating model...")
-        y_pred = rf_model.predict(X_test)
-        mae = mean_absolute_error(y_test, y_pred)
-        r2 = r2_score(y_test, y_pred)
-        
-        # Log metrics to MLflow
-        mlflow.log_metric("mae", mae)
-        mlflow.log_metric("r2", r2)
-        
-        # Calculate Feature Importance
-        importance_df = pd.DataFrame({
-            "Feature": X_train.columns,
-            "Importance": rf_model.feature_importances_
-        }).sort_values("Importance", ascending=False)
-        print("\nTop 3 Important Features:")
-        print(importance_df.head(3).to_string(index=False))
-        
+
+        #Log model attribute
+        y_pred = rf_model.predict(X)
+        signature = infer_signature(X, y_pred)
+        mlflow.sklearn.log_model(sk_model=rf_model,name="RF_Tuned", signature=signature, registered_model_name="RF_Tuned")
+                
         # Save the model and feature names artifacts locally for the FastAPI app
         os.makedirs("../models", exist_ok=True)
-        joblib.dump(rf_model, "../models/random_forest_baseline.pkl")
-        joblib.dump(list(X_train.columns), "../models/feature_names.pkl")
+        joblib.dump(rf_model, "../models/random_forest_tuned.pkl")
+        joblib.dump(list(X.columns), "../models/feature_names_tuned.pkl")
         
-        # Log the model to MLflow's artifact store
-        mlflow.sklearn.log_model(rf_model, artifact_path="model")
-        
-        print(f"\nRun completed! MAE: {mae:.4f}, R2: {r2:.4f}")
+        print(f"\nRun completed! MAE: {np.mean(mae_scores):.4f}, R2: {np.mean(r2_scores):.4f}")
 
 if __name__ == "__main__":
     # Ensure the script is run from the src/ directory
